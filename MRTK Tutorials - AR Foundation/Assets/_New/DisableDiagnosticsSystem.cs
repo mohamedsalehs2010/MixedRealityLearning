16746b6b9ea8b598a176657eab9b00f91df26bab
Martin Wilter <martinwilter@gmail.com>
2020-05-17 15:01:26 -0700

    Feat: Disable diagnostics system for Android and iOS at runtime
    DisableDiagnosticsSystem.

---
+++ MRTK Tutorials - AR Foundation/Assets/_New/DisableDiagnosticsSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+
+public class DisableDiagnosticsSystem : MonoBehaviour
+{
+#if UNITY_ANDROID || UNITY_IOS
+    [SerializeField]
+    [Header("Android & iOS Settings")]
+    bool disableDiagnosticsSystem = true;
+
+    void Start()
+    {
+        if (disableDiagnosticsSystem)
+        {
+            CoreServices.DiagnosticsSystem.ShowDiagnostics = false;
+        }
+    }
+#endif
+}
