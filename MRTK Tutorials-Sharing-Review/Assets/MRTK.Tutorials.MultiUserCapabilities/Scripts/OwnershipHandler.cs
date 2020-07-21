be69e44039f30469d3c29766e14291f23978949e
paul <paul,gumpert@onginnovations.com>
2020-07-21 15:05:15 -0700

    sharing-02: 3/12

---
+++ MRTK Tutorials-Sharing-Review/Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/OwnershipHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.MixedReality.Toolkit.Input;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace MRTK.Tutorials.MultiUserCapabilities
+{
+    [RequireComponent(typeof(PhotonView), typeof(GenericNetSync))]
+    public class OwnershipHandler : MonoBehaviourPun, IPunOwnershipCallbacks, IMixedRealityInputHandler
+    {
+        public void OnInputDown(InputEventData eventData)
+        {
+            photonView.RequestOwnership();
+        }
+
+        public void OnInputUp(InputEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+        {
+            targetView.TransferOwnership(requestingPlayer);
+        }
+
+        public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TransferControl(Player idPlayer)
+        {
+            if (photonView.IsMine) photonView.TransferOwnership(idPlayer);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (photonView != null) photonView.RequestOwnership();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RequestOwnership()
+        {
+            photonView.RequestOwnership();
+        }
+    }
+}
